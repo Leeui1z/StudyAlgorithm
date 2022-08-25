baf6bdb32fc1eb7460c93465387e87afea812355
Leeui1z <euikyoung.esther.lee95@gmail.com>
2022-08-26 02:10:56 +0900

    알고리즘과 퀴즈 1단계 21번 문제

---
+++ StudyAlgorithm/_220825.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class _220825
+{
+    //기초알고리즘 강의 수강 후 새로운 레포지토리에 응용 연습 시작
+    //[?] 변수를 제시했을 때 x의 값 구하기
+    
+    static void Main()
+    {
+        int i=6; int j=8;
+        if (i>0&&j>0)
+        {
+            j += i++;
+        }
+        Console.WriteLine($"i의 값 : {i}, j의 값 : {j}");
+    }
+}
+
