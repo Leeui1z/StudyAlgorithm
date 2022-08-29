489bc7e2eab26c7dbebcb330202694dce543add1
Leeui1z <euikyoung.esther.lee95@gmail.com>
2022-08-29 22:27:27 +0900

    백준 2438번 N개의 별 찍기

--- StudyAlgorithm/_220829_no2438.cs
+++ StudyAlgorithm/_220829_no2438.cs
@@ -12,16 +12,15 @@ class _220829_no2438
         //[1] Input : 1<N=<100
         int n = int.Parse(Console.ReadLine());
         //[2] progress : 
-        for (int i = 1; i < n; i++)
+        for (int i = 1; i < n; i++)//n회 까지 반복
         {
-            for (int j = 0; j < i; j++)
+            for (int j = 0; j < i; j++)//n회까지 반복하는 동안 n까지 숫자만큼 *찍는 반복문
             {
                 Console.Write("*");
             }
             Console.WriteLine();
+            //[3] Output :첫번째 줄부터 N번쨰 줄까지 차례대로 별을 출력한다.
         }
-        
-        //[3] Output :첫번째 줄부터 N번쨰 줄까지 차례대로 별을 출력한다.
 
     }
 }
