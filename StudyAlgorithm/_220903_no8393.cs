3f561daa58d6114a7854f470dd01800dd57bfd9b
Leeui1z <euikyoung.esther.lee95@gmail.com>
2022-09-03 23:55:00 +0900

    백준 8393번 1부터 N까지의 합

---
+++ StudyAlgorithm/_220903_no8393.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class _220903_no8393
+{
+    static void Main()
+    {
+        //n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램 작성하라
+        int n = int.Parse(Console.ReadLine());//n이 주어졌을 때.
+        int sum = 0;
+        for (int i = 1; i < n; i++)
+        {
+            sum += i;
+        }
+        Console.WriteLine($"1부터 {n}까지의 합: {sum}");
+
+    }
+}
+
