87475389cd07af99cb5e6a013ce2f43e2be1de00
Leeui1z <euikyoung.esther.lee95@gmail.com>
2022-09-07 00:53:23 +0900

    백준 각 자리의 숫자 합계

---
+++ StudyAlgorithm/_220907_no11720.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class _220907_no11720
+{
+    //[?] 공백없이 쓰여진 n개의 숫자를 합하여 출력
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        //공백없이 숫자를 받는다.
+        //반복문으로 int인수에 앞숫자부터 뒷수자까지 +=한다.
+        int sum = 0;
+        for (int i = 0; i < x; i++)
+        {
+            sum += x;
+        }
+        Console.WriteLine($"총 합계 :{sum}");
+
+    }
+}
+
