478783f7b2bd8f89c114e0ef6a205400cfaa31b8
Leeui1z <euikyoung.esther.lee95@gmail.com>
2022-09-08 23:56:10 +0900

    백준 공백없이 쓰인 N개의 숫자 합계

--- StudyAlgorithm/_220907_no11720.cs
+++ StudyAlgorithm/_220907_no11720.cs
@@ -10,12 +10,15 @@ class _220907_no11720
     static void Main()
     {
         int x = int.Parse(Console.ReadLine());
+        string str = Console.ReadLine();
+        char[] num = str.ToCharArray();
         //공백없이 숫자를 받는다.
-        //반복문으로 int인수에 앞숫자부터 뒷수자까지 +=한다.
+
         int sum = 0;
+        //반복문으로 int인수에 앞숫자부터 뒷수자까지 +=한다.
         for (int i = 0; i < x; i++)
         {
-            sum += x;
+            sum += num[i] -'0';
         }
         Console.WriteLine($"총 합계 :{sum}");
 
