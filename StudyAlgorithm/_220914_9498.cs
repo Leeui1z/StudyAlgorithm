2575fa1641418170fb2ca9f68c919d45357c8c7d
Leeui1z <euikyoung.esther.lee95@gmail.com>
2022-09-15 23:53:29 +0900

    시험 점수에 대한 성적 출력

--- StudyAlgorithm/_220914_9498.cs
+++ StudyAlgorithm/_220914_9498.cs
@@ -4,9 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StudyAlgorithm
+
+class _220914_9498
 {
-    class _220914_9498
+    //시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램
+    static void Main()
     {
+        int score = int.Parse(Console.ReadLine());
+        if (score >= 90) Console.WriteLine("A");
+        else if (score >= 80) Console.WriteLine("B");
+        else if (score >= 70) Console.WriteLine("C");
+        else if (score >= 60) Console.WriteLine("D");
+        else Console.WriteLine("F");
     }
+
 }
+
