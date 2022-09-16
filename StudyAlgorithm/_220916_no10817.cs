0e4da2d3fdcc3f2968864733fb956e1ea4486f0b
Leeui1z <euikyoung.esther.lee95@gmail.com>
2022-09-16 22:55:50 +0900

    세 수중 두번째 큰 값 출력하기 -진행중

---
+++ StudyAlgorithm/_220916_no10817.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class _220916_no10817
+{
+    //입력받은 세 수 중 두번째로 큰 정수를 출력
+    //a,b,c를 입력받아 배열에 넣는다.
+    //for 문으로 배열을 순차적으로 훑되 큰 것을 앞으로 위치변환을 시킨다.
+    //배열 중 두번째칸에 들어간 값을 출력한다.
+    static void Main()
+    {
+
+    }
+}
+
